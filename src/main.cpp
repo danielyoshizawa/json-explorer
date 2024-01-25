83975443de0ce658c00ad492c36b9deea53f92e4
daniel.yoshizawa <yoshidanielcwb@gmail.com>
2024-01-25 10:08:27 -0300

    #3 - Adding libcurl dependency

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,22 @@
 // Program entry point
 
 #include <iostream>
+#include <curl/curl.h>
 
 int main()
 {
   std::cout << "Say hello to my little friend!!!" << std::endl;
+
+  // Testing libcurl
+  CURL *curl = curl_easy_init();
+  if (curl) {
+    CURLcode response;
+    curl_easy_setopt(curl, CURLOPT_URL, "https://dog-api.kinduff.com/api/facts");
+    response = curl_easy_perform(curl); // By default curl_easy_perform will print the page to the console
+    curl_easy_cleanup(curl);
+  }
+
+  std::cout << std::endl;
 
   return 0;
 }
